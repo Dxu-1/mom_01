dc53dfa49d10c7e81df63e4372404efc97bc0655
dxx <dxx@dxx.com>
2022-01-01 19:23:44 +0800

    first commit

---
+++ src/main/java/com/xd/utils/DateUtil.java
@@ -0,0 +1,30 @@
+package com.xd.utils;
+
+import com.xd.pojo.JsonResult;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+/**
+*@author xd
+*@create 2022/1/1
+*@description 
+*/
+public class DateUtil {
+
+    public static Object stringDate(String date){
+        LocalDate localDate;
+        if (date == null){
+            localDate = LocalDate.now();
+        }else{
+            try {
+                localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            }catch (DateTimeParseException e){
+                return JsonResult.error("时间格式不对");
+            }
+        }
+        return localDate;
+    }
+
+}
